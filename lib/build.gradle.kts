15161d83fd3f3d4fbc6974040057e87f37136392
Trent Summerlin <whirvis@ardenus.org>
2024-10-30 22:04:40 -0500

    Initial commit for JRaknet v3.0

---
+++ lib/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.whirvis"
+            artifactId = "jraknet"
+            version = "3.0.0-SNAPSHOT"
+
+            from(components["java"])
+        }
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "JRakNet"
+include("lib")
