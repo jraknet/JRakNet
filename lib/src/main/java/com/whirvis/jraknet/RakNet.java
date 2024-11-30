8b64dbfb5e17c0dda89869e9c179ceaf5eb34d3f
Trent Summerlin <whirvis@ardenus.org>
2024-11-30 14:34:01 -0600

    Add getMTU() to RakNet class

--- lib/src/main/java/com/whirvis/jraknet/RakNet.java
+++ lib/src/main/java/com/whirvis/jraknet/RakNet.java
@@ -29,6 +29,14 @@
  */
 package com.whirvis.jraknet;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Objects;
+
 public final class RakNet {
 
     /**
@@ -66,6 +74,98 @@ public final class RakNet {
             }
         }
         return false;
+    }
+
+    /**
+     * The minimum value for the maximum transfer unit (MTU) allowed
+     * by RakNet.
+     * <p>
+     * The MTU size determines how many bytes can be sent in a single
+     * packet before it is split up when sent over the wire.
+     *
+     * @see #getMTU(InetAddress)
+     */
+    public static final int MINIMUM_MTU_SIZE = 400;
+
+    /**
+     * The maximum value for the maximum transfer unit (MTU) allowed
+     * by RakNet.
+     * <p>
+     * The MTU size determines how many bytes can be sent in a single
+     * packet before it is split up when sent over the wire.
+     *
+     * @see #getMTU(InetAddress)
+     */
+    public static final int MAXIMUM_MTU_SIZE = 1492;
+
+    /**
+     * Returns the MTU for the network card associated with a given address.
+     * <p>
+     * The minimum value that can be returned is {@value #MINIMUM_MTU_SIZE}.
+     * If the network card has an MTU lower than that, an exception will be
+     * thrown.
+     * The largest value that can be returned is {@value #MAXIMUM_MTU_SIZE};
+     * even if the network card has a higher MTU. This is to remain aligned
+     * with the specifications of RakNet.
+     *
+     * @param address the address bound to the network card.
+     * @return the MTU for the network card.
+     * @throws NullPointerException  if {@code address} is {@code null}.
+     * @throws RakNetSocketException if no network card is bound to the given
+     *                               address; if the MTU for the network card
+     *                               is less than {@value #MINIMUM_MTU_SIZE}.
+     */
+    public static int getMTU(@NotNull InetAddress address) {
+        Objects.requireNonNull(address, "address cannot be null");
+
+        NetworkInterface nit;
+        try {
+            nit = NetworkInterface.getByInetAddress(address);
+        } catch (SocketException e) {
+            throw new RakNetSocketException(e);
+        }
+
+        if (nit == null) {
+            String msg = "No network card for " + address;
+            throw new RakNetSocketException(msg);
+        }
+
+        int mtu;
+        try {
+            mtu = nit.getMTU();
+        } catch (SocketException e) {
+            throw new RakNetSocketException(e);
+        }
+
+        /* probably won't happen, check just to be safe */
+        if (mtu < MINIMUM_MTU_SIZE) {
+            String msg = "MTU for " + address + " too low (" + mtu + ")";
+            throw new RakNetSocketException(msg);
+        }
+
+        return Math.min(mtu, MAXIMUM_MTU_SIZE);
+    }
+
+    /**
+     * Returns the MTU for the network card associated with a given address.
+     * <p>
+     * The minimum value that can be returned is {@value #MINIMUM_MTU_SIZE}.
+     * If the network card has an MTU lower than that, an exception will be
+     * thrown.
+     * The largest value that can be returned is {@value #MAXIMUM_MTU_SIZE};
+     * even if the network card has a higher MTU. This is to remain aligned
+     * with the specifications of RakNet.
+     *
+     * @param address the address bound to the network card.
+     * @return the MTU for the network card.
+     * @throws NullPointerException  if {@code address} is {@code null}.
+     * @throws RakNetSocketException if no network card is bound to the given
+     *                               address; if the MTU for the network card
+     *                               is less than {@value #MINIMUM_MTU_SIZE}.
+     */
+    public static int getMTU(@NotNull InetSocketAddress address) {
+        Objects.requireNonNull(address, "address cannot be null");
+        return getMTU(address.getAddress());
     }
 
     private RakNet() {
